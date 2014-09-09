1f5f9b9067937f8a578886d3cc61bb0ca13750a9
Remi Rampin <remirampin@gmail.com>
2014-09-09 15:33:45 -0400

    Removes new dumpToFile() formats

--- viewer.py
+++ viewer.py
@@ -16,8 +16,6 @@ class TableCell(SpreadsheetCell):
 
 
 class TableCellWidget(QCellWidget):
-    save_formats = QCellWidget.save_formats + ["HTML files (*.html)"]
-
     def __init__(self, parent=None):
         QCellWidget.__init__(self, parent)
 
@@ -68,56 +66,6 @@ class TableCellWidget(QCellWidget):
         self.table.setSortingEnabled(True)
         self.table.sortByColumn(0, QtCore.Qt.AscendingOrder)
         self.table.resizeColumnsToContents()
-
-    def write_html(self):
-        document = ['<!DOCTYPE html>\n'
-                    '<html>\n  <head>\n'
-                    '    <meta http-equiv="Content-type" content="text/html; '
-                            'charset=utf-8" />\n'
-                    '    <title>Exported table</title>\n'
-                    '    <style type="text/css">\n'
-                    'table { border-collapse: collapse; }\n'
-                    'td, th { border: 1px solid black; }\n'
-                    '    </style>\n'
-                    '  </head>\n  <body>\n    <table>\n']
-        table = self.orig_table
-        if table.names is not None:
-            names = table.names
-        else:
-            names = ['col %d' % n for n in xrange(table.columns)]
-        document.append('<tr>\n')
-        document.extend('  <th>%s</th>\n' % name for name in names)
-        document.append('</tr>\n')
-        columns = [table.get_column(col) for col in xrange(table.columns)]
-        for row in xrange(table.rows):
-            document.append('<tr>\n')
-            for col in xrange(table.columns):
-                elem = columns[col][row]
-                if isinstance(elem, bytes):
-                    elem = elem.decode('utf-8', 'replace')
-                elif not isinstance(elem, unicode):
-                    elem = unicode(elem)
-                document.append('  <td>%s</td>\n' % elem)
-            document.append('</tr>\n')
-        document.append('    </table>\n  </body>\n</html>\n')
-
-        return ''.join(document)
-
-    def dumpToFile(self, filename):
-        ext = os.path.splitext(filename)[1].lower()
-        if ext in ('.html', '.htm'):
-            with open(filename, 'wb') as fp:
-                fp.write(self.write_html())
-        else:
-            super(TableCellWidget, self).dumpToFile(filename)
-
-    def saveToPDF(self, filename):
-        document = QtGui.QTextDocument()
-        document.setHtml(self.write_html())
-        printer = QtGui.QPrinter()
-        printer.setOutputFormat(QtGui.QPrinter.PdfFormat)
-        printer.setOutputFileName(filename)
-        document.print_(printer)
 
 
 _modules = [TableCell]
